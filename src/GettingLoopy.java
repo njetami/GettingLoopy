1fcd7a225b3b089703b593036cd62f34ff99a3c6
Njeta Melvin Ndi <njetami@mail.uc.edu>
2022-07-08 14:39:05 -0400

    Initial commit

---
+++ src/GettingLoopy.java
@@ -0,0 +1,116 @@
+public class GettingLoopy {
+    public static void main(String[] args) {
+/*
+//	1. A loop that counts (i.e. displays the count) up by 1 from 0 to 30
+
+        for(int cnt = 0; cnt <= 30; cnt = cnt+1)
+        {
+            System.out.println(cnt);
+        }
+
+ */
+/*
+        //2. loop that counts down by 1 from 30 to 0
+
+        for(int cnt = 30; cnt >= 0; cnt=cnt-1)
+        {
+            System.out.println(cnt);
+            }
+ */
+/*
+          //3. loop that counts up by 3 from 0 to 18
+        for(int cnt = 0; cnt <= 18; cnt=cnt+3)
+        {
+            System.out.println(cnt);
+        }
+
+ */
+        /*
+        //4.A loop that counts down by 2 from 10 to 0
+
+        for(int cnt = 10; cnt >= 0; cnt=cnt-2)
+        {
+            System.out.println(cnt);
+        }
+        */
+
+
+
+
+         /*
+           //	5. A nested loop that creates this figure (there are no blank lines)
+         int Row=5;
+         int Col=1;
+         int row=1;
+         int col=1;
+
+
+
+        for(row = 1; row <=Row; row=row+1)
+        {
+            for (col=1; col<=Col; col=col+1)
+            {
+            System.out.print("*");
+            }
+            System.out.println("");Col++;
+        }
+
+          */
+
+
+         /*
+          //6. A nested loop that creates this figure (there are no blank lines)
+
+        int Row=5;
+        int Col=5;
+        int row;
+        int col;
+
+
+
+        for(row =1; row <=Row; row=row+1)
+        {
+            System.out.println();
+            for (col=1; col<=Col; col=col+1)
+            {
+                System.out.print("*");
+            }
+            Col--;
+
+        }
+
+          */
+
+
+        /*
+        //	7. A nested loop that creates this figure (there are no blank lines)
+        int Row=5;
+        int Col=5;
+        int row;
+        int col;
+
+
+
+        for(row =1; row <=Row; row=row+1)
+        {
+            System.out.println();
+            for (col=1; col<=Col; col=col+1)
+            {
+                System.out.print("*");
+            }
+
+
+
+        }
+
+         */
+
+
+
+
+
+
+    }
+}
+
+
